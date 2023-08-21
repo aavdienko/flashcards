4576f239022aa2f0c2839db7ed493ca7408e6c30
aavdienko <asiavostok@seznam.cz>
2023-08-21 20:06:26 +0200

    add polymorphic typography component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Button } from './components/ui'
+import { Button, Typography } from './components/ui'
 
 export function App() {
   return (
     <div>
       <Button as={'button'}>Hello</Button>
+      <div>
+        <Typography variant={'large'}>What is Curriculum Development ? </Typography>
+      </div>
     </div>
   )
 }
